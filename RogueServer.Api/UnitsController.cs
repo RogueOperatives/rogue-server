c8bb74bfc69e8ffd26b13c7f9cb7c926d4f5b7d8
Philipp Dreher <dreher.philipp@googlemail.com>
2017-06-04 22:18:33 +0200

    WebAPI basics

---
+++ RogueServer.Api/UnitsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using RogueEngine.Core.Contracts;
+using RogueEngine.DataAccess;
+
+namespace RogueServer.Api
+{
+    [Route("api/[controller]")]
+    public class UnitsController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<IUnit> Get()
+        {
+            return RepositoryIndex.Instance.UnitRepository.GetAllUnits();
+        }
+
+        [HttpGet("{id}")]
+        public IUnit Get(string id)
+        {
+            return RepositoryIndex.Instance.UnitRepository.GetUnitById(Guid.Parse(id));
+        }
+    }
+}
